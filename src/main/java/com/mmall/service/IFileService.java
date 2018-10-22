9535658a795a806f4d4a44841b4cc685fc25e965
fanhao <fanhaohf@163.com>
2018-10-22 23:45:15 +0800

    富文本上传功能

--- src/main/java/com/mmall/service/IFileService.java
+++ src/main/java/com/mmall/service/IFileService.java
@@ -1,9 +1,14 @@
 package com.mmall.service;
 
+import org.springframework.web.multipart.MultipartFile;
+
 /**
  * @Author: fanhao
  * @Date: 2018/9/19 23:39
  * @Version 1.0
  */
 public interface IFileService {
+
+    String upload(MultipartFile file, String path);
+
 }
